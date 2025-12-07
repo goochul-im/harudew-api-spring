1ea41cfae54af6555a65fd4c6745278ff8349e8b
goochul-im <goochul175465@gmail.com>
2025-12-07 22:03:07 +0900

    docs(activity): 주석 추가

--- src/main/kotlin/b1a4/harudew/activity/domain/Activity.kt
+++ src/main/kotlin/b1a4/harudew/activity/domain/Activity.kt
@@ -4,6 +4,16 @@ import b1a4.harudew.diary.domain.Diary
 import java.time.LocalDate
 import java.time.LocalDateTime
 
+/**
+ * @param id
+ * @param content 내용
+ * @param vector Qdrant 벡터
+ * @param createdAt 생성 시간
+ * @param diary 이 활동이 있는 일기
+ * @param strength 이 활동에 대한 강점
+ * @param weakness 이 활동에 대한 약점
+ * @param cluster 클러스터
+ */
 class Activity(
     val id: Long,
     val content: String,
--- src/main/kotlin/b1a4/harudew/activity/domain/ActivityCluster.kt
+++ src/main/kotlin/b1a4/harudew/activity/domain/ActivityCluster.kt
@@ -2,6 +2,13 @@ package b1a4.harudew.activity.domain
 
 import b1a4.harudew.member.domain.Member
 
+/**
+ * @param id
+ * @param author 활동 주인
+ * @param clusteredCount 클러스터 카운터
+ * @param label 레이블
+ * @param centroid 벡터
+ */
 class ActivityCluster(
     val id: Long,
     val author: Member,
