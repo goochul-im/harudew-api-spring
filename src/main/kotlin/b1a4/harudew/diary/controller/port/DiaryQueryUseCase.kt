ef73045ef5cb537e3aae4e59f9480c3f20951980
goochul-im <goochul175465@gmail.com>
2025-12-12 22:00:07 +0900

    in-progress: Response 작성 중

--- src/main/kotlin/b1a4/harudew/diary/controller/port/DiaryQueryUseCase.kt
+++ src/main/kotlin/b1a4/harudew/diary/controller/port/DiaryQueryUseCase.kt
@@ -1,12 +1,23 @@
 package b1a4.harudew.diary.controller.port
 
 import b1a4.harudew.diary.controller.port.dto.out.DiaryAnalysisResponse
+import b1a4.harudew.diary.controller.port.dto.out.DiaryJsonResponse
 import b1a4.harudew.diary.domain.Diary
+import b1a4.harudew.member.domain.Member
+import java.time.LocalDate
 
 interface DiaryQueryUseCase {
 
-    fun findById(id: Long): DiaryAnalysisResponse
+    fun findById(diaryId: Long, member: Member): DiaryAnalysisResponse
 
-    fun findJsonById(id: Long): DiaryJsonResponse
+    fun findJsonById(diaryId: Long, member: Member): DiaryJsonResponse
+
+    fun findInfoByDate(diaryId: Long, member: Member, date: LocalDate): DiaryInfoResponse
+
+    fun search(member: Member, query: String): DiarySearchResponse
+
+    fun findAll(member: Member): DiaryPageResponse
+
+    fun findBookmark(member: Member): DiaryPageResponse
 
 }
--- src/main/kotlin/b1a4/harudew/diary/controller/port/dto/out/DiaryAnalysisResponse.kt
+++ src/main/kotlin/b1a4/harudew/diary/controller/port/dto/out/DiaryAnalysisResponse.kt
@@ -4,6 +4,7 @@ import b1a4.harudew.activity.port.out.ActivitySimpleResponse
 import b1a4.harudew.emotion.port.out.EmotionAnalysisResponse
 import b1a4.harudew.person.port.out.PeopleAnalysisResponse
 import b1a4.harudew.todo.port.out.TodoAnalysisResponse
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class DiaryAnalysisResponse(
     val id: Long,
@@ -11,8 +12,11 @@ data class DiaryAnalysisResponse(
     val photoPath: List<String>?,
     val content: String,
     val people: List<PeopleAnalysisResponse>,
-    val selfEmotion: List<EmotionAnalysisResponse>,
-    val stateEmotion: List<EmotionAnalysisResponse>,
-    val activity: List<ActivitySimpleResponse>,
+    @field:JsonProperty("selfEmotion")
+    val selfEmotions: List<EmotionAnalysisResponse>,
+    @field:JsonProperty("stateEmotion")
+    val stateEmotions: List<EmotionAnalysisResponse>,
+    @field:JsonProperty("activity")
+    val activities: List<ActivitySimpleResponse>,
     val todos: List<TodoAnalysisResponse>
 )
